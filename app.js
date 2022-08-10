fda74772a8e4afcf105de57e90840f493c5e1645
Hasnain Qari <qarihasnain43@gmail.com>
2022-08-10 10:16:43 +0500

    Animation2add

--- app.js
+++ app.js
@@ -27,35 +27,35 @@ window.onkeydown = function () {
     }, 1000);
   }
 };
-var count1 = 350;
-window.onkeydown = function () {
-  console.log(event.keyCode);
-  var character2 = document.getElementById("character2");
-  if (event.keyCode === 76) {
-    character2.src = "animation/action3.gif";
-    count1 = count1 + 50;
-    console.log(count1);
-    character2.style.left = count1 + "px";
-    setTimeout(function () {
-      character2.src = "animation/four.gif";
-    }, 100);
-  }
-  if (event.keyCode === 82) {
-    character2.src = "animation/action3.gif";
-    count1 = count1 - 50;
-    console.log(count1);
-    character2.style.left = count1 + "px";
-    setTimeout(function () {
-      character2.src = "animation/four.gif";
-    }, 100);
-  }
-  if (event.keyCode === 70) {
-    character2.src = "animation/bala2.gif";
-    setTimeout(function () {
-      character2.src = "animation/four.gif";
-    }, 1000);
-  }
-};
+// var count1 = 350;
+// window.onkeydown = function () {
+//   console.log(event.keyCode);
+//   var character2 = document.getElementById("character2");
+//   if (event.keyCode === 76) {
+//     character2.src = "animation/action3.gif";
+//     count1 = count1 + 50;
+//     console.log(count1);
+//     character2.style.left = count1 + "px";
+//     setTimeout(function () {
+//       character2.src = "animation/four.gif";
+//     }, 100);
+//   }
+//   if (event.keyCode === 82) {
+//     character2.src = "animation/action3.gif";
+//     count1 = count1 - 50;
+//     console.log(count1);
+//     character2.style.left = count1 + "px";
+//     setTimeout(function () {
+//       character2.src = "animation/four.gif";
+//     }, 100);
+//   }
+//   if (event.keyCode === 70) {
+//     character2.src = "animation/bala2.gif";
+//     setTimeout(function () {
+//       character2.src = "animation/four.gif";
+//     }, 1000);
+//   }
+// };
 
 // window.onkeyup = function (){
 //     console.log(event.keyCode);
